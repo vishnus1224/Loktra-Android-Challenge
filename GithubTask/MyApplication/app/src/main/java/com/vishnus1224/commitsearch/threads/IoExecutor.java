d7b87042d85bbefb8395edf089faa1297669263e
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-07-16 16:27:37 +0530

    renamed from scheduler to executor

--- GithubTask/MyApplication/app/src/main/java/com/vishnus1224/commitsearch/threads/IoExecutor.java
+++ GithubTask/MyApplication/app/src/main/java/com/vishnus1224/commitsearch/threads/IoExecutor.java
@@ -1,5 +1,7 @@
 package com.vishnus1224.commitsearch.threads;
 
+import javax.inject.Inject;
+
 import rx.Scheduler;
 import rx.schedulers.Schedulers;
 
@@ -7,7 +9,11 @@ import rx.schedulers.Schedulers;
  * Provides the IO scheduler.
  * Created by Vishnu on 7/16/2016.
  */
-public class IoScheduler implements BaseScheduler {
+public class IoExecutor implements BaseExecutor {
+
+    @Inject
+    public IoExecutor() {
+    }
 
     @Override
     public Scheduler scheduler() {
