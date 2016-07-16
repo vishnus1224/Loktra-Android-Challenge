657ce20df3032c058a6433958fda1c9b6e681b3f
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-07-16 15:26:14 +0530

    custom scope for objects that live as long as the application

---
+++ GithubTask/MyApplication/app/src/main/java/com/vishnus1224/commitsearch/di/scope/PerApplication.java
@@ -0,0 +1,14 @@
+package com.vishnus1224.commitsearch.di.scope;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Vishnu on 7/16/2016.
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerApplication {
+}
