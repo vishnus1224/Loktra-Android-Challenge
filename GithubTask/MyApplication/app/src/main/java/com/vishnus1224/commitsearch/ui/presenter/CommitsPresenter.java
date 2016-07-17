097a7ddc6bf0cd2071b2b47a3f528373d85384a8
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-07-17 14:45:57 +0530

    commits presenter and its test

---
+++ GithubTask/MyApplication/app/src/main/java/com/vishnus1224/commitsearch/ui/presenter/CommitsPresenter.java
@@ -0,0 +1,91 @@
+package com.vishnus1224.commitsearch.ui.presenter;
+
+import com.vishnus1224.commitsearch.model.CommitWrapper;
+import com.vishnus1224.commitsearch.model.QueryParamPage;
+import com.vishnus1224.commitsearch.ui.view.CommitsView;
+import com.vishnus1224.commitsearch.usecase.UseCase;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import rx.Subscriber;
+
+/**
+ * Handles all the operations related to the commits activity.
+ * All interactions on the view by the user will be passed
+ * to the presenter to take correct actions.
+ * Created by Vishnu on 7/16/2016.
+ */
+public class CommitsPresenter {
+
+    private CommitsView commitsView;
+
+    private UseCase useCase;
+
+    private QueryParamPage queryParamPage;
+
+    @Inject
+    public CommitsPresenter(@Named("getCommits") UseCase useCase, QueryParamPage queryParamPage) {
+
+        this.useCase = useCase;
+
+        this.queryParamPage = queryParamPage;
+
+    }
+
+    public void onViewAttached(CommitsView commitsView){
+
+        this.commitsView = commitsView;
+
+    }
+
+    public void onViewDetached(){
+
+        useCase.unSubscribe();
+
+    }
+
+    public void fetchCommits(){
+
+        commitsView.showProgressBar();
+
+        useCase.execute(queryParamPage.getPageNumber(), new CommitWrapperSubscriber());
+
+    }
+
+    public void loadCommitsOnNextPage(){
+
+        queryParamPage = queryParamPage.nextPage();
+
+        useCase.execute(queryParamPage.getPageNumber(), new CommitWrapperSubscriber());
+
+    }
+
+    private final class CommitWrapperSubscriber extends Subscriber<List<CommitWrapper>> {
+
+
+        @Override
+        public void onCompleted() {
+
+            commitsView.hideProgressBar();
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+            commitsView.hideProgressBar();
+
+        }
+
+        @Override
+        public void onNext(List<CommitWrapper> commitWrapperList) {
+
+            commitsView.showCommits(commitWrapperList);
+
+        }
+    }
+
+}
---
+++ GithubTask/MyApplication/app/src/test/java/com/vishnus1224/commitsearch/ui/presenter/CommitsPresenterTest.java
@@ -0,0 +1,66 @@
+package com.vishnus1224.commitsearch.ui.presenter;
+
+import com.vishnus1224.commitsearch.model.QueryParamPage;
+import com.vishnus1224.commitsearch.ui.view.CommitsView;
+import com.vishnus1224.commitsearch.usecase.UseCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import rx.Subscriber;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Vishnu on 7/17/2016.
+ */
+public class CommitsPresenterTest {
+
+    private final int FAKE_PAGE = 445;
+
+    @Mock
+    CommitsView commitsView;
+
+    @Mock
+    UseCase useCase;
+
+    @Mock
+    QueryParamPage queryParamPage;
+
+    private CommitsPresenter commitsPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+
+        commitsPresenter = new CommitsPresenter(useCase, queryParamPage);
+
+        commitsPresenter.onViewAttached(commitsView);
+
+    }
+
+    @Test
+    public void testFetchCommits() throws Exception {
+
+        commitsPresenter.fetchCommits();
+
+        verify(commitsView).showProgressBar();
+        verify(useCase).execute(anyInt(), any(Subscriber.class));
+    }
+
+    @Test
+    public void testLoadCommitsOnNextPage() throws Exception {
+
+        when(queryParamPage.nextPage()).thenReturn(new QueryParamPage(FAKE_PAGE));
+
+        commitsPresenter.loadCommitsOnNextPage();
+
+        verify(queryParamPage).nextPage();
+        verify(useCase).execute(anyInt(), any(Subscriber.class));
+
+    }
+}
