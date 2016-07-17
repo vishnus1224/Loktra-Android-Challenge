6ec76c4b36e5889ff5592d8858745e3459fc7f15
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-07-17 23:17:13 +0530

    created the layout for the activity and setup the views

--- FlickrTask/FlickFlipper/app/src/main/java/com/vishnus1224/flickflipper/ui/activity/PhotoStreamActivity.java
+++ FlickrTask/FlickFlipper/app/src/main/java/com/vishnus1224/flickflipper/ui/activity/PhotoStreamActivity.java
@@ -1,15 +1,48 @@
 package com.vishnus1224.flickflipper.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ProgressBar;
 
 import com.vishnus1224.flickflipper.R;
+import com.vishnus1224.flickflipper.di.component.ActivityComponent;
+import com.vishnus1224.flickflipper.di.component.DaggerActivityComponent;
+import com.vishnus1224.flickflipper.di.module.ActivityModule;
+
 
 public class PhotoStreamActivity extends BaseActivity {
+
+    private GridView photoStreamGridView;
+    private ProgressBar progressBar;
+
+    private ActivityComponent activityComponent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_stream);
+
+        setupViews();
+
+        injectDependencies();
+    }
+
+    private void setupViews() {
+
+        photoStreamGridView = (GridView) findViewById(R.id.photoStreamGridView);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+    }
+
+    private void injectDependencies() {
+
+        activityComponent = DaggerActivityComponent.builder()
+                .applicationComponent(getApplicationComponent())
+                .activityModule(new ActivityModule(this))
+                .build();
+
+        activityComponent.inject(this);
+
     }
 }
