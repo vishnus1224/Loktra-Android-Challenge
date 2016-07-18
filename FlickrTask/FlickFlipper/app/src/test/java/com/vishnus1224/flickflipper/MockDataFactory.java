5153a898c3687618d1addd28611f1151ab009363
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-07-18 19:37:00 +0530

    used for mocking models

---
+++ FlickrTask/FlickFlipper/app/src/test/java/com/vishnus1224/flickflipper/MockDataFactory.java
@@ -0,0 +1,45 @@
+package com.vishnus1224.flickflipper;
+
+import com.vishnus1224.flickflipper.model.PhotoInfo;
+import com.vishnus1224.flickflipper.model.PhotoInfoMedia;
+import com.vishnus1224.flickflipper.model.PhotoInfoWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Vishnu on 7/18/2016.
+ */
+public class MockDataFactory {
+
+    public static final PhotoInfoWrapper mockPhotoInfoWrapperList(){
+
+        List<PhotoInfo> photoInfoList = new ArrayList<>();
+
+        photoInfoList.add(mockPhotoInfo());
+
+        PhotoInfoWrapper photoInfoWrapper = new PhotoInfoWrapper(photoInfoList);
+
+
+        return photoInfoWrapper;
+
+    }
+
+    public static final PhotoInfo mockPhotoInfo(){
+
+        PhotoInfoMedia photoInfoMedia = mockPhotoInfoMedia();
+
+        PhotoInfo photoInfo = new PhotoInfo("Amazed", "bin/media/image", photoInfoMedia, "test description");
+
+        return photoInfo;
+
+    }
+
+    public static final PhotoInfoMedia mockPhotoInfoMedia(){
+
+        PhotoInfoMedia photoInfoMedia = new PhotoInfoMedia("volumes/data/src");
+
+        return photoInfoMedia;
+
+    }
+}
