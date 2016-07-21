a4a1d2471e088c4d9a629321fe7d926434e2ac70
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-07-21 21:41:43 +0530

    reset view state to prevent issues due to view reuse

--- FlickrTask/FlickFlipper/app/src/main/java/com/vishnus1224/flickflipper/ui/adapter/PhotoStreamGridAdapter.java
+++ FlickrTask/FlickFlipper/app/src/main/java/com/vishnus1224/flickflipper/ui/adapter/PhotoStreamGridAdapter.java
@@ -1,7 +1,5 @@
 package com.vishnus1224.flickflipper.ui.adapter;
 
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,12 +29,6 @@ public class PhotoStreamGridAdapter extends BaseAdapter {
 
     private Activity activity;
 
-    private AnimatorSet flipOutAnimation;
-
-    private AnimatorSet flipInAnimation;
-
-    private AnimatorSet resetAnimation;
-
     @Inject
     public PhotoStreamGridAdapter(Activity activity, LayoutInflater layoutInflater) {
 
@@ -44,11 +36,6 @@ public class PhotoStreamGridAdapter extends BaseAdapter {
 
         this.layoutInflater = layoutInflater;
 
-        flipOutAnimation = (AnimatorSet) AnimatorInflater.loadAnimator(activity, R.animator.flip_out_animator);
-
-        flipInAnimation = (AnimatorSet) AnimatorInflater.loadAnimator(activity, R.animator.flip_in_animator);
-
-        resetAnimation = (AnimatorSet) AnimatorInflater.loadAnimator(activity, R.animator.reset_animator);
     }
 
     public void updateDataSet(List<PhotoInfo> photoInfoList){
@@ -103,53 +90,53 @@ public class PhotoStreamGridAdapter extends BaseAdapter {
 
         final PhotoInfo photoInfo = (PhotoInfo) getItem(i);
 
+        if(photoInfo.getVisibleSide() == null || photoInfo.getVisibleSide() == PhotoInfo.Side.FRONT){
+
+            setViewAlpha(0, photoStreamViewHolder.backView);
+            setViewRotationY(-180, photoStreamViewHolder.backView);
+
+            setViewAlpha(1.0f, photoStreamViewHolder.frontView);
+            setViewRotationY(0, photoStreamViewHolder.frontView);
+
+        }else{
+
+            setViewAlpha(1.0f, photoStreamViewHolder.backView);
+            setViewRotationY(0, photoStreamViewHolder.backView);
+
+            setViewAlpha(0.0f, photoStreamViewHolder.frontView);
+            setViewRotationY(180, photoStreamViewHolder.frontView);
+
+
+        }
+
         //load the image with picasso.
         Picasso.with(activity).load(photoInfo.getMedia().getMediaUrl()).placeholder(android.R.color.darker_gray).fit().into(photoStreamViewHolder.frontView);
 
         photoStreamViewHolder.backViewPhotoTitle.setText(photoInfo.getTitle());
 
-        photoStreamViewHolder.backViewPhotoDimens.setText(photoInfo.getLink());
-
-        //animate the layout when clicked.
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                animateViews(photoInfo, photoStreamViewHolder.frontView, photoStreamViewHolder.backView);
-
-            }
-        });
+        photoStreamViewHolder.backViewPhotoDimens.setText(photoInfo.getPhotoMetaData().getWidth() + "X"
+                + photoInfo.getPhotoMetaData().getHeight());
 
         return view;
     }
 
-    private void animateViews(PhotoInfo photoInfo, ImageView frontView, View backView) {
+    private void setViewAlpha(float alpha, View view) {
 
-        if(photoInfo.getVisibleSide() == PhotoInfo.Side.FRONT || photoInfo.getVisibleSide() == null){
+        view.setAlpha(alpha);
 
-            flipOutAnimation.setTarget(frontView);
-            flipInAnimation.setTarget(backView);
-
-            photoInfo.setVisibleSide(PhotoInfo.Side.BACK);
-
-        }else{
-
-            flipOutAnimation.setTarget(backView);
-            flipInAnimation.setTarget(frontView);
-
-            photoInfo.setVisibleSide(PhotoInfo.Side.FRONT);
-
-        }
-
-        flipOutAnimation.start();
-
-        flipInAnimation.start();
     }
 
-    private static class PhotoStreamViewHolder{
+    private void setViewRotationY(int rotationAngle, View view){
 
-        ImageView frontView;
-        RelativeLayout backView;
+        view.setRotationY(rotationAngle);
+
+    }
+
+
+    public static class PhotoStreamViewHolder{
+
+        public ImageView frontView;
+        public RelativeLayout backView;
         TextView backViewPhotoTitle;
         TextView backViewPhotoDimens;
 
